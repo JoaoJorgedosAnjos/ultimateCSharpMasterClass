a0db314c4e27cdbceed69eb9356dcab5050f3cfd
João Jorge <joaojorgedosanjos@gmail.com>
2025-10-14 21:06:57 -0300

    feat: adiciona solução do exercício Properties of the Order

--- OurFirstClass71/Classes/Rectangle.cs
+++ OurFirstClass71/Classes/Rectangle.cs
@@ -4,30 +4,76 @@ namespace OurFirstClass.Classes;
 
 public class Rectangle
 {
+    private const int NumberOfSides = 4;
     public int Width;
-    public int Height;
+    private int _height;
 
     public Rectangle(int width, int height)
     {
-        Width = width;
-        Height = height;
+        Width = GetLenghtOrDefault(width, nameof(Width));
+        _height = GetLenghtOrDefault(height, nameof(_height));
+    }
+
+    public int GetHeight => _height;
+
+    public void SetHeigth(int value)
+    {
+        if (value > 0)
+        {
+            _height = value;
+        }
+    }
+
+    private int GetLenghtOrDefault(int lenght, string name)
+    {
+        const int defaultValueIfNonPositive = 1;
+        if (lenght <= 0)
+        {
+            _height = defaultValueIfNonPositive;
+        }
+
+        return lenght;
     }
 }
 
 class ShapesMeasurementsCalculator
 {
     //Expression-bodied methods
-    public int CalculateRectanglePerimeter(Rectangle rectangle) => 2 * rectangle.Width + 2 * rectangle.Height;
-    
+    public int CalculateRectanglePerimeter(Rectangle rectangle) => 2 * rectangle.Width + 2 * rectangle.GetHeight;
+
     // public int CalculateRectanglePerimeter(Rectangle rectangle)
     // {
     //     return 2 * rectangle.Width + 2 * rectangle.Height;
     // }
     //Expression-bodied methods
-    public int CalculateRectangleArea(Rectangle rectangle) => rectangle.Width * rectangle.Height;
-    
+    public int CalculateRectangleArea(Rectangle rectangle) => rectangle.Width * rectangle.GetHeight;
+
     // public int CalculateRectangleArea(Rectangle rectangle)
     // {
     //     return rectangle.Width * rectangle.Height;
     // }
 }
+
+// No C#, GetHeight é uma propriedade do tipo "expressão de corpo"(expression - bodied property), e não um método, por isso
+//     não é chamado com parênteses().Por que usar GetHeight e não GetHeight() ? GetHeight foi declarado assim:
+//
+// csharp
+// public int GetHeight => _height;
+//
+// Isso define uma propriedade de somente leitura que retorna o valor de _height.
+//     Propriedades são acessadas como campos, sem usar parênteses, ao contrário de métodos.
+//     Se fosse um método, seria declarado assim:
+//
+// csharp
+//
+// public int GetHeight()
+// {
+//     return _height;
+// }
+//
+// e aí você precisaria chamar com os parênteses, ou seja, GetHeight().Resumo
+//
+// Propriedades em C# são acessadas sem(), pois se comportam como campos calculados.
+//     Métodos precisam ser chamados com().Usar propriedades para acessar valores privados é uma prática comum
+//     em C#, oferecendo encapsulamento sem parecer um método explícito.No seu código, GetHeight é uma
+//     propriedade, por isso você usa rectangle.GetHeight e não GetHeight().
--- OurFirstClass71/Program.cs
+++ OurFirstClass71/Program.cs
@@ -3,7 +3,7 @@
 var rectangle = new Rectangle(4, 8);
 var calculator = new ShapesMeasurementsCalculator();
 Console.WriteLine("Width is " + rectangle.Width);
-Console.WriteLine("Height is " + rectangle.Height);
+Console.WriteLine("Height is " + rectangle.GetHeight);
 
 Console.WriteLine(calculator.CalculateRectanglePerimeter(rectangle));
 Console.WriteLine(calculator.CalculateRectangleArea(rectangle));
@@ -13,7 +13,7 @@ Console.WriteLine();
 var calculator2 = new ShapesMeasurementsCalculator();
 var rectangle2 = new Rectangle(5,10);
 Console.WriteLine("Width is " + rectangle2.Width);
-Console.WriteLine("Height is " + rectangle2.Height);
+Console.WriteLine("Height is " + rectangle2.GetHeight);
 
 Console.WriteLine(calculator2.CalculateRectanglePerimeter(rectangle2));
 Console.WriteLine(calculator2.CalculateRectangleArea(rectangle2));
