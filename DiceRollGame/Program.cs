1aaee661edc92313bc8f5485fcb12d9ce8293f22
João Jorge <joaojorgedosanjos@gmail.com>
2025-10-17 13:20:20 -0300

    feat: adiciona game Dice Roll Game

---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DiceRollGame.Game;
+
+var random = new Random();
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+
+GameResult gameResult = guessingGame.Play();
+guessingGame.PrintResult(gameResult);
